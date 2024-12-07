96e7fb2e6385eff21a98851f1953f4f0fc930039
sikrierdanila@yandex.ru <sikrierdanila@yandex.ru>
2024-12-07 15:23:28 +0300

    Updates

---
+++ precache-manifest.9ec35437389ecf026141ee9abd599ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d97b23a6a15b8745646a6f42912a0",
+    "url": "/vpnproject/index.html"
+  },
+  {
+    "revision": "496d01765d85850ca37e",
+    "url": "/vpnproject/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/vpnproject/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "496d01765d85850ca37e",
+    "url": "/vpnproject/static/js/main.0c1229ed.chunk.js"
+  },
+  {
+    "revision": "39918cb0b761827cb6df",
+    "url": "/vpnproject/static/js/runtime~main.6145ceaa.js"
+  }
+]);
